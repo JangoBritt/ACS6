195c0f0e7d70f396c37e3e3a34620e30fa4fae1e
Adam Davidson <adam@jangobritt.com>
2024-06-23 17:14:51 +0100

    FD fix

--- behavior_packs/FarmersDelight B/scripts/data/recipe/cookRecipe.js
+++ behavior_packs/FarmersDelight B/scripts/data/recipe/cookRecipe.js
@@ -1,14 +1,5 @@
 const vanillaItemList = [
-    "minecraft:beef",
-    "minecraft:chicken",
-    "minecraft:cod",
-    "minecraft:egg",
-    "minecraft:beef",
-    "minecraft:mutton",
-    "minecraft:porkchop",
-    "minecraft:potato",
-    "minecraft:rabbit",
-    "minecraft:salmon"
+
 ];
 export { vanillaItemList };
 //# sourceMappingURL=cookRecipe.js.map
